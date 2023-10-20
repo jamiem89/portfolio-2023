b866052c4429aab673138423ed3203b6c29a1340
Jamie Moore <jamiemoore2689@gmail.com>
2023-10-20 14:52:18 +0100

    cleared out project - prep for rebuild

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,13 @@
 import { useState, useEffect } from 'react'
 
 import Head from 'next/head'
-import HomeHero from '../components/HomeHero';
-import Splash from '../components/Splash';
-import StickyNav from '../components/StickyNav';
-import Footer from '../global/Footer';
 
 export default function Home() {
 
-  const [splashEnd, setSplashEnd] = useState(false);
-
-  // Window resize event to set screen size
-
-  const [deviceHeight, setDeviceHeight] = useState(null);
-
-  useEffect(() => {
-    setDeviceHeight(`${window.innerHeight}px`)
-  });
-
   return (
     <>
-      <Head>
-        <title>Jamie Moore - Frontend Developer</title>
-        <meta name="description" content="UK Based frontend developer specialising in custom Wordpress themes." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={splashEnd ? '' : 'splash'} style={{ "--device-height": deviceHeight }}>
-        <Splash setSplashEnd={setSplashEnd}/>
-        <HomeHero />
-        <StickyNav />
-        <Footer />
+      <Head />
+      <main>
       </main>
     </>
   )
